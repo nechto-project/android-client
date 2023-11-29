20b7072d635843698396e876b281e9aa79c39c7e
radkoff26 <radko.slava@mail.ru>
2023-11-29 22:10:03 +0300

    change base url, change icon

--- app/src/main/java/com/github/radkoff26/nechto/Constants.kt
+++ app/src/main/java/com/github/radkoff26/nechto/Constants.kt
@@ -1,5 +1,5 @@
 package com.github.radkoff26.nechto
 
 object Constants {
-    const val BASE_URL = "https://d18a-31-134-188-96.ngrok-free.app"
+    const val BASE_URL = "https://556c-188-162-64-182.ngrok-free.app"
 }
